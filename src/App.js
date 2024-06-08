f86d6d4ba3a69bc2f430b8522367f7f33a8a740f
abdellatief <abdellatiefhamed00@gmail.com>
2024-06-08 19:17:23 +0300

    changing app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import MyLogIn from "./commonComp/MyLogIn";
 import BasicCard from "./commonComp/cart";
-import ShowImages from "./commonComp/ShowImages";
-import MiniDrawer from "./commonComp/common/sidebar";
 import SearchImages from "./Pages/SearchImages";
 import { BrowserRouter, Route, Switch, Routes } from "react-router-dom";
-import Favourites from "./commonComp/favourites";
 import Fav from "./Pages/fav";
 
 function App() {
